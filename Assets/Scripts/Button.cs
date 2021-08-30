e324d19e57f8cad632ff80a960228613455398da
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2021-08-30 15:59:58 -0500

    Updated Button Script

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
+using System;
 
 public class Button : MonoBehaviour
 {
     public bool type; //true is for light buttons, false is for heavy buttons
     [SerializeField]
     Transform button;
-    [SerializeField]
-    BoxCollider collider;
 
     public Vector3 restPos;
 
     bool isTouching = false;
+
+    public static Action Activate = delegate { };
     /*Light Buttons:
      * - Can be pressed by any creature or object
      * - Will stay pressed once pressed
@@ -27,7 +28,7 @@ public class Button : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        restPos = collider.bounds.max;
+        restPos = button.position;
     }
 
     // Update is called once per frame
@@ -39,17 +40,35 @@ public class Button : MonoBehaviour
         }
         else
         {
-            
+            if(!isTouching)
+            {
+                Debug.Log("Behind LeanTween");
+                LeanTween.moveY(this.gameObject, restPos.y, 1);
+            }
         }
 
-        if(collider.bounds.max.y < restPos.y)
+        if(GetComponent<Collider>().bounds.max.y != restPos.y)
         {
-            
+            Activate();
         }
     }
 
+
     private void OnCollisionEnter(Collision collision)
     {
-        
+        isTouching = true;
+        Debug.Log("Is Touching");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        StartCoroutine(WaitForTouch());
+    }
+
+    IEnumerator WaitForTouch()
+    {
+        yield return new WaitForSeconds(4);
+        isTouching = false;
+        Debug.Log("Is not Touching");
     }
 }
--- Assets/Scripts/Control/Land/LandMovement.cs
+++ Assets/Scripts/Control/Land/LandMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
-using System;
 
 public class LandMovement : MonoBehaviour
 {
