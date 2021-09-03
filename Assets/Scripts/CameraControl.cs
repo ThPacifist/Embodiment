f0017b4ec632310ee696450465a4e0a097e5e337
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2021-09-03 17:24:40 -0500

    Finished Camera Script

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -11,32 +11,64 @@ public class CameraControl : MonoBehaviour
 
     public GameObject topRight;
     public GameObject botLeft;
-    
+    public bool withinX = true;
+    public bool withinY = true;
+
+    Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
+        center = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0));
         CenterCamera();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(CheckBotLeft() || CheckTopRight())
+        center = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0));
+        if (CheckPosX() || CheckPosY())
         {
-            CenterCamera();
+            if(CheckPosX() && !CheckPosY()) // Cam excedes the x limit
+            {
+                CenterCamera("x");
+            }
+            if(!CheckPosX() && CheckPosY()) // Cam excedes the y limit
+            {
+                CenterCamera("y");
+            }
+            if(CheckPosX() && CheckPosY()) // Cam does not excedes any limit
+            {
+                CenterCamera();
+            }
         }
     }
 
-    void CenterCamera()
+    void CenterCamera(string change = "all")
     {
-        cam.transform.position = new Vector3(player.position.x, player.position.y, cam.transform.position.z);
+        if (change == "all")
+        {
+            cam.transform.position = new Vector3(player.position.x, player.position.y, cam.transform.position.z);
+        }
+        else if (change == "x")
+        {
+            cam.transform.position = new Vector3(player.position.x, cam.transform.position.y, cam.transform.position.z);
+        }
+        else if (change == "y")
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x, player.position.y, cam.transform.position.z);
+        }
     }
 
-    bool CheckTopRight()
+    bool CheckPosX()
     {
-        if(cam.ViewportToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0)).x > topRight.transform.position.x ||
-           cam.ViewportToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0)).y > topRight.transform.position.y)
+        if(cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0)).x > topRight.transform.position.x)
         {
+            ReAdjustX("+x");
+            return false;
+        }
+        else if(cam.ScreenToWorldPoint(Vector3.zero).x < botLeft.transform.position.x)
+        {
+            ReAdjustX("-x");
             return false;
         }
         else
@@ -45,22 +77,68 @@ public class CameraControl : MonoBehaviour
         }
     }
 
-    bool CheckBotLeft()
+    bool CheckPosY()
     {
-        if(cam.ViewportToWorldPoint(Vector3.zero).x < botLeft.transform.position.x ||
-           cam.ViewportToWorldPoint(Vector3.zero).y < botLeft.transform.position.y)
+        if(cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0)).y > topRight.transform.position.y)
         {
+            ReAdjustY("+y");
+            return false;
+        }
+        else if (cam.ScreenToWorldPoint(Vector3.zero).y < botLeft.transform.position.y)
+        {
+            ReAdjustY("-y");
             return false;
         }
         else
         {
             return true;
         }
-        
     }
 
-    bool CheckCamera()
+    void ReAdjustX(string posX)
     {
-        return true;
+        if(posX == "+x")
+        {
+            if(player.position.x < center.x)
+            {
+                CenterCamera();
+            }
+        }
+        else if(posX == "-x")
+        {
+            if(player.position.x > center.x)
+            {
+                CenterCamera();
+            }
+        }
+    }
+
+    void ReAdjustY(string posY)
+    {
+        if(posY == "+y")
+        {
+            if(player.position.y < center.y)
+            {
+                CenterCamera();
+            }
+        }
+        else if(posY == "-y")
+        {
+            if(player.position.y > center.y)
+            {
+                CenterCamera();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        //Top Right Lines
+        Gizmos.DrawLine(topRight.transform.position, new Vector3(botLeft.transform.position.x, topRight.transform.position.y, 0));
+        Gizmos.DrawLine(topRight.transform.position, new Vector3( topRight.transform.position.x, botLeft.transform.position.y, 0));
+        //Bottom Left Lines
+        Gizmos.DrawLine(botLeft.transform.position, new Vector3(botLeft.transform.position.x, topRight.transform.position.y, 0));
+        Gizmos.DrawLine(botLeft.transform.position, new Vector3(topRight.transform.position.x, botLeft.transform.position.y, 0));
     }
 }
