cca45ffdbafa42f65ce3e0650a521335a51b1997
TheRedSpy43 <61850423+TheRedSpy43@users.noreply.github.com>
2021-09-16 16:29:38 -0500

    Title screen basic scripts
    Description

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Created by Jason on 9/14
+    /*
+     * TODO:
+     * Add direct functionality to these buttons
+     */
+
+     //Public variables and assets
+    public string newGameSecne;
+    public Transform[] menuButtons;
+    public Transform[] settingsButtons;
+
+    //Private variable
+
+
+    //Start new game
+    public void newGame()
     {
-        
+        //Load first level/cutscene
+        SceneManager.LoadScene(newGameSecne);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Continue game
+    public void continueGame()
     {
-        
+        //Load saved game from however that works
+    }
+
+    //Go to settings menu
+    public void settings()
+    {
+        //Enable settings buttons
+        for (int i = 0; i < settingsButtons.Length; i++)
+        {
+            settingsButtons[i].gameObject.SetActive(true);
+        }
+        //Disable menu buttons
+        for (int i = 0; i < menuButtons.Length; i++)
+        {
+            menuButtons[i].gameObject.SetActive(false);
+        }
+    }
+
+    //Quit Game
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    //Back to menu
+    public void backToMenu()
+    {
+        //Enable menu buttons
+        for(int i = 0; i < menuButtons.Length; i++)
+        {
+            menuButtons[i].gameObject.SetActive(true);
+        }
+        //Disable settings buttons
+        for(int i = 0; i < settingsButtons.Length; i++)
+        {
+            settingsButtons[i].gameObject.SetActive(false);
+        }
     }
 }
