3a902c9df20edc0c59f570b584dae915db7ec159
Capta1nKa1ser <isaacrk@venturecomm.net>
2021-09-22 08:40:46 -0500

    Tile Set Wackiness

--- Assets/Scripts/Control/PlayerControls.cs
+++ Assets/Scripts/Control/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Control/Player/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Control/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
