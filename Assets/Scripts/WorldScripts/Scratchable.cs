592eef33b78056ceb25f66e0e34e947cafa36167
TheRedSpy43 <61850423+TheRedSpy43@users.noreply.github.com>
2021-10-26 18:01:24 -0500

    Scratchable object script base
    Still doesn't work, but the base is written

--- Assets/Scripts/WorldScripts/Scratchable.cs
+++ Assets/Scripts/WorldScripts/Scratchable.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Scratchable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * Description:
+     * This script makes an object breakable by the cat scratch.
+     * - The bool 'breakObj' will make the object break instead of switching sprites
+     * - Note that the object does not have to be the same as the object hit by the cat
+     */
+
+    //Public variables and assets
+    public GameObject scratableObj;
+    public int health;
+    public bool breakObj = false;
+
+    //Private variables
+
+
+    //This function will break the object
+    private void breakObject()
     {
-        
+        //Break the object/play animation to break object
+        Destroy(scratableObj);
+        //Disable this script for if this is not a part of the object
     }
 
-    // Update is called once per frame
-    void Update()
+    //Gets called when this object is attacked
+    private void attacked()
     {
-        
+        //Reduce health
+        health--;
+        //Check if it is done
+        if(health <= 0)
+        {
+            //Break object
+            if(breakObj)
+            {
+                breakObject();
+            }
+            else
+            {
+                //switchSprites();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Check if it is the attack
+        if(collision.CompareTag("PlayerAttack"))
+        {
+            attacked();
+        }
     }
 }
