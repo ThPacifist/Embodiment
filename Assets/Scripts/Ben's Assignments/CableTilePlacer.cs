539b0611b5d2ba340cd2cc7caa21a5b717c8d6a5
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2022-01-28 15:44:11 -0600

    Update CableTilePlacer.cs

--- Assets/Scripts/Ben's Assignments/CableTilePlacer.cs
+++ Assets/Scripts/Ben's Assignments/CableTilePlacer.cs
@@ -2,17 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TileMapColorChanger))]
 public class CableTilePlacer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2Int currentPos;
+    [SerializeField]
+    GameObject startPos;
+    [SerializeField]
+    GameObject endPos;
+
+    Vector2Int startPosInt;
+    Vector2Int endPosInt;
+
+    void GenerateCable()
     {
-        
+        //Converts the positions of the game objects to be integer vectors
+        startPosInt = Vector2Int.FloorToInt(startPos.transform.position);
+        endPosInt = Vector2Int.FloorToInt(endPos.transform.position);
+        //Initializes starting position
+        currentPos = startPosInt;
+
+        while(currentPos.x > endPosInt.x)
+        {
+            currentPos.x += MoveTowardsInt(currentPos, endPosInt);
+        }
+
+        Debug.Log("X position is now the same");
     }
 
-    // Update is called once per frame
-    void Update()
+    //Shorthand for Vector2Int.FloorToInt
+    Vector2Int FlInt(Vector2 input)
     {
-        
+        return Vector2Int.FloorToInt(input);
+    }
+
+    int MoveTowardsInt(Vector2 a, Vector2 b)
+    {
+        float dist = Vector2.Distance(a, b);
+
+        if(dist < 0)
+        {
+            return 1;
+        }
+        else if(dist > 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3Int sPint = Vector3Int.FloorToInt(startPos.transform.position);
+        Vector3Int ePint = Vector3Int.FloorToInt(endPos.transform.position);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(sPint, Vector3.one * 0.3f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(ePint, Vector3.one * 0.3f);
     }
 }
