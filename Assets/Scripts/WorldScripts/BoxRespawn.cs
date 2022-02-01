b2f0aadd1a9b8e2221002d35a63977d9f0667069
TheRedSpy43 <61850423+TheRedSpy43@users.noreply.github.com>
2022-02-01 13:19:21 -0600

    Box respawn

---
+++ Assets/Scripts/WorldScripts/BoxRespawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRespawn : MonoBehaviour
+{
+    //Public variables
+    public Transform respawnPos;
+
+    //When it enters any of these triggers, reapwawn the box
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (other.tag == "Death")
+        {
+            this.transform.position = respawnPos.position;
+            this.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        
+    }
+}
