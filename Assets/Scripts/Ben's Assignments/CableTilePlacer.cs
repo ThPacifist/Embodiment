f5ec53e724106120f1f22188e84e982f8bdb2ccd
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2022-02-14 08:05:15 -0600

    Update to Cable Tile Placer

--- Assets/Scripts/Ben's Assignments/CableTilePlacer.cs
+++ Assets/Scripts/Ben's Assignments/CableTilePlacer.cs
@@ -10,7 +10,7 @@ public class CableTilePlacer : MonoBehaviour
 {
     [SerializeField]
     LineRenderer line;
-    Vector3 currentPos;
+    Vector3Int currentPos;
     [SerializeField]
     GameObject startPos;
     [SerializeField]
@@ -18,40 +18,96 @@ public class CableTilePlacer : MonoBehaviour
     [SerializeField]
     Tilemap tileMap;
 
-    Vector3 startPosR;
-    Vector3 endPosR;
+    Vector3Int startPosR;
+    Vector3Int endPosR;
+    //Serves as the next position of the loop
+    Vector3Int nextPos;
     List<Vector3> Positions = new List<Vector3>();
+    int count = 0;
 
     public void GenerateCable()
     {
-        Positions.Clear();
         //Converts the positions of the game objects to be integer vectors
-        startPosR = Vector3Int.FloorToInt(startPos.transform.position) + new Vector3(0.5f, 0.5f, 0);
-        endPosR = Vector3Int.FloorToInt(endPos.transform.position) + new Vector3(0.5f, 0.5f, 0);
+        startPosR = Vector3Int.FloorToInt(startPos.transform.position);
+        startPosR.z = 0;
+        endPosR = Vector3Int.FloorToInt(endPos.transform.position);
+        endPosR.z = 0;
         Debug.Log("Start Pos is " + startPosR + ". End pos is " + endPosR + ".");
         //Initializes starting position
         currentPos = startPosR;
+        nextPos = currentPos;
         Positions.Add(currentPos);
 
-        while(currentPos.x != endPosR.x)
+        while(currentPos.x != endPosR.x && count < 100)
         {
-            currentPos.x += MoveTowardsInt(currentPos.x, endPosR.x);
+            nextPos.x += MoveTowardsInt(currentPos.x, endPosR.x);
+            if (tileMap.GetTile(nextPos))
+            {
+                Debug.Log("There is a tile");
+                nextPos = currentPos;
+                //If there is not a tile above, move current pos up
+                if(!tileMap.GetTile(nextPos + Vector3Int.up))
+                {
+                    currentPos.y += 1;
+                }
+                //If there is, move down
+                else
+                {
+                    currentPos.y -= 1;
+                }
+            }
+            else
+            {
+                Debug.Log("There is not a tile");
+                currentPos.x += MoveTowardsInt(currentPos.x, endPosR.x);
+            }
+
             Positions.Add(currentPos);
+            nextPos = currentPos;
+
+            count++;
         }
 
-        while (currentPos.y != endPosR.y)
+        nextPos = currentPos;
+        count = 0;
+
+        while (currentPos.y != endPosR.y && count < 100)
         {
-            currentPos.y += MoveTowardsInt(currentPos.y, endPosR.y);
+            nextPos.y += MoveTowardsInt(currentPos.y, endPosR.y);
+            if (tileMap.GetTile(nextPos))
+            {
+                Debug.Log("There is a tile");
+                nextPos = currentPos;
+                //If there is not a tile to the right, move current pos up
+                if (!tileMap.GetTile(nextPos + Vector3Int.right))
+                {
+                    currentPos.x += 1;
+                }
+                //If there is, move left
+                else
+                {
+                    currentPos.x -= 1;
+                }
+            }
+            else
+            {
+                Debug.Log("There is not a tile");
+                currentPos.y += MoveTowardsInt(currentPos.y, endPosR.y);
+            }
+
             Positions.Add(currentPos);
+            count++;
         }
 
-        for(int i = 0; i < Positions.Count; i++)
+        /*for(int i = 0; i < Positions.Count; i++)
         {
             Debug.Log("Position " + i + " is: " + Positions[i]);
-        }
-
-        line.positionCount = Positions.Count;
-        line.SetPositions(Positions.ToArray());
+        }*/
+        //if (count >= 50)
+        //{
+            line.positionCount = Positions.Count;
+            line.SetPositions(Positions.ToArray());
+        //}
 
         Positions.Clear();
     }
@@ -77,8 +133,8 @@ public class CableTilePlacer : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Vector3 sPint = Vector3Int.FloorToInt(startPos.transform.position) + new Vector3(0.5f, 0.5f, 0);
-        Vector3 ePint = Vector3Int.FloorToInt(endPos.transform.position) + new Vector3(0.5f, 0.5f, 0);
+        Vector3 sPint = Vector3Int.FloorToInt(startPos.transform.position);
+        Vector3 ePint = Vector3Int.FloorToInt(endPos.transform.position);
 
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(sPint, 1);
