cd8caa8a4237e615209c92aa61c453b3ba8d78d2
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2022-02-15 14:28:41 -0600

    Starting Scripts for other form controllers

---
+++ Assets/Scripts/Control/FishController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : Controller
+{
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
---
+++ Assets/Scripts/Control/HumanController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : Controller
+{
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
