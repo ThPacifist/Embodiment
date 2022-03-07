726f3b4a32638cd4f8db04bce472baf241cec44d
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2022-03-07 16:46:05 -0600

    Added Animations to Expozyte

--- Assets/ExpozyteAnimator.cs
+++ Assets/ExpozyteAnimator.cs
@@ -5,13 +5,67 @@ using UnityEngine;
 public class ExpozyteAnimator : MonoBehaviour
 {
     public Animator expAnim;
-    public GameObject Player;
     public float angle;
 
     private void Update()
     {
-        Vector2 direction = transform.position - Player.transform.position;
+        float distance = Vector3.Distance(transform.position, PlayerBrain.PB.transform.position);
+        Vector2 direction = transform.position - PlayerBrain.PB.transform.position;
 
         angle = Vector2.SignedAngle(Vector2.up, direction);
+
+        if (distance > 0.5f)
+        {
+            if ((angle > 0 && angle < 22.5) || (angle < 0 && angle > -22.5)) //Player is below the expozyte
+            {
+                Debug.Log("The Blob is below");
+                expAnim.SetInteger("Direction", 8);
+            }
+            else if (angle < -22.5 && angle > -67.5)
+            {
+                Debug.Log("The Blob is bottom left");
+                expAnim.SetInteger("Direction", 7);
+            }
+            else if (angle < -67.5 && angle > -112.5)
+            {
+                Debug.Log("The Blob is to the left");
+                expAnim.SetInteger("Direction", 4);
+            }
+            else if (angle < -112.5 && angle < -157.5)
+            {
+                Debug.Log("The Blob is top Left");
+                expAnim.SetInteger("Direction", 1);
+            }
+            else if ((angle > -157.5 && angle < -180) || (angle > 157.5 && angle < 180))
+            {
+                Debug.Log("The Blob is above");
+                expAnim.SetInteger("Direction", 2);
+            }
+            else if (angle > 112.5 && angle < 157.5)
+            {
+                Debug.Log("The Blob is top right");
+                expAnim.SetInteger("Direction", 3);
+            }
+            else if (angle > 67.5 && angle < 112.5)
+            {
+                Debug.Log("The Blob is to the right");
+                expAnim.SetInteger("Direction", 6);
+            }
+            else if (angle > 22.5 && angle < 67.5)
+            {
+                Debug.Log("The Blob is bottom right");
+                expAnim.SetInteger("Direction", 9);
+            }
+            else
+            {
+                Debug.Log("The Expozyte is looking forward");
+                expAnim.SetInteger("Direction", 5);
+            }
+        }
+        else
+        {
+            Debug.Log("The Expozyte is looking forward");
+            expAnim.SetInteger("Direction", 5);
+        }
     }
 }
