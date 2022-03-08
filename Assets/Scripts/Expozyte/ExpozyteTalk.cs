3bb4bb346ee3d4dfa05ddffe8af5f180148727e9
TheRedSpy43 <61850423+TheRedSpy43@users.noreply.github.com>
2022-03-07 18:31:27 -0600

    Start on letter by letter talking
    Reworking the entire expozyte talk while im at it

--- Assets/Scripts/Expozyte/ExpozyteTalk.cs
+++ Assets/Scripts/Expozyte/ExpozyteTalk.cs
@@ -11,13 +11,23 @@ public class ExpozyteTalk : MonoBehaviour
      * It is attached to the point he is at rather than him to ensure the player can see the text at the correct spot
      */
 
+    //Structures
+    public struct dialogueStruct
+    {
+        public string[] Dialogue;
+        public int[] timeDisplayed;
+    };
+
     //Public variables
-    public TextMeshPro textBox;
-    public string[] Dialogue;
-    public int[] timeDisplayed;
+    public TextMeshProUGUI textBox;
+    public GameObject bubble;
+    public dialogueStruct checkpointDialogue = new dialogueStruct();
 
     //Private variables
+    private string currentText;
+    private int checkpoint;
     private int currentDialogue = 0;
+    private int letter = 0;
 
     //Detects when expozyte enters the checkpoint
     public void OnTriggerEnter2D(Collider2D other)
@@ -30,11 +40,11 @@ public class ExpozyteTalk : MonoBehaviour
     private void PlayDialogue()
     {
         //Check if there is anything else to display
-        if (currentDialogue < Dialogue.Length)
+        if (currentDialogue < checkpointDialogue.Dialogue[currentDialogue].Length)
         {
             //Change text, make it visible and wait
-            textBox.text = Dialogue[currentDialogue];
-            textBox.gameObject.SetActive(true);
+            OneChar();
+            bubble.gameObject.SetActive(true);
             StartCoroutine("Wait");
         }
     }
@@ -43,12 +53,32 @@ public class ExpozyteTalk : MonoBehaviour
     IEnumerator Wait()
     {
         //Keep it displayed
-        yield return new WaitForSeconds(timeDisplayed[currentDialogue]);
-        textBox.gameObject.SetActive(false);
+        yield return new WaitForSeconds(currentText.Length + 2);
+        bubble.gameObject.SetActive(false);
         //Turn it off for a little bit
         yield return new WaitForSeconds(0.5f);
+        //Clear the textbox
+        textBox.text = "";
+        letter = 0;
         //Put the next one up
         currentDialogue++;
-        PlayDialogue();
+        if (currentDialogue < checkpointDialogue.Dialogue.Length)
+        {
+            currentText = checkpointDialogue.Dialogue[currentDialogue];
+            PlayDialogue();
+        }
+    }
+
+    //Put the text in there one character at a time
+    IEnumerator OneChar()
+    {
+        while (letter < currentText.Length)
+        {
+            //Put the next letter in and increment letter
+            textBox.text += currentText[letter];
+            letter++;
+            //Next frame
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
